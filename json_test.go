8a34abe0733c6f383d62ee6db62459647b11430c
Mark Wolfe <mark@wolfe.id.au>
2023-05-14 19:12:07 +1000

    chore(tests): added coverage for execute function

--- json_test.go
+++ json_test.go
@@ -1,6 +1,7 @@
 package jsontemplate
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,4 +16,22 @@ func TestExecute(t *testing.T) {
 	res, err := tpl.ExecuteToString([]byte(`{"msg":{"name":"markw","age":23,"cyclist":true}}`))
 	assert.NoError(err)
 	assert.Equal(`{"data":"markw",count:23,"flag":true}`, res)
+}
+
+func TestTemplate_Execute(t *testing.T) {
+	assert := require.New(t)
+
+	content := `{"greeting":${name}}`
+	tmpl, err := NewTemplate(content)
+	assert.NoError(err)
+
+	evt := []byte(`{"name": "John"}`)
+	buf := new(bytes.Buffer)
+	n, err := tmpl.Execute(buf, evt)
+	assert.NoError(err)
+
+	expectedDoc := `{"greeting":"John"}`
+
+	assert.Equal(int64(len(expectedDoc)), n)
+	assert.Equal(expectedDoc, buf.String())
 }
